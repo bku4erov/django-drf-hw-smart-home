c87bb65dad243d066d0837da89131faa8fa03348
Boris Kucherov <boris.ku4erov@gmail.com>
2023-09-21 20:40:33 +0300

    add database settings

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from measurement import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_smart_home',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': config.DB_HOST,
+        'PORT': config.DB_PORT,
+        'USER': config.DB_USERNAME,
+        'PASSWORD': config.DB_PASSWORD,
     }
 }
 
