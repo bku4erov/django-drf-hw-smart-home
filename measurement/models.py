8cf6149f8b8ffdc77e389f1906deec1c555a46a3
Boris Kucherov <boris.ku4erov@gmail.com>
2023-09-21 21:14:38 +0300

    add basic serializers

--- measurement/models.py
+++ measurement/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=2000, null=True)
--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,3 +1,15 @@
+from models import Measurement, Sensor
 from rest_framework import serializers
 
+
 # TODO: опишите необходимые сериализаторы
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'sensor', 'temperature', 'created_at']
